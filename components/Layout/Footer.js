063001b77fba1e15c835cfec4790bd7aa13a2e1a
Mubashirtaj <165596982+Mubashirtaj@users.noreply.github.com>
2024-12-23 23:19:43 +0500

    Update Footer.js

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -53,7 +53,7 @@ const Footer = () => {
           <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
           <ul className="text-black-500">
             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              LaslesVPN ?{" "}
+              TrimVPN ?{" "}
             </li>
             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
               FAQ{" "}
